7f296e7d0dfa22a8a3887e5ef7b3871b980788a0
Diego F. Prado <goidesdesign@gmail.com>
2021-05-16 13:30:38 -0500

    add ID for selectors and get inputs via JS

--- js/main.js
+++ js/main.js
@@ -1,2 +1,8 @@
 const compose = (...functions) => data =>
   functions.reduceRight((value, func) => func(value), data)
+
+
+const description = document.querySelector('#description');
+const calores = document.querySelector('#calores'); 
+const carbs = document.querySelector('#carbs');
+const protein = document.querySelector('#protein');
